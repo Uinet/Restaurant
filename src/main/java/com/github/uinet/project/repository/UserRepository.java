403cf909594d1356897c9073def3d2702b12a3cd
Uinet <malcevdimitriy@gmail.com>
2021-08-04 17:23:00 +0300

    Added JPA Repository Interface UserRepository

---
+++ src/main/java/com/github/uinet/project/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.github.uinet.project.repository;
+
+import com.github.uinet.project.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long>{
+    Optional<User> findByEmail(String email);
+}
